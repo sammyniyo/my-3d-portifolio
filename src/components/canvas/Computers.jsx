28d009f80b57b7f22d110a32b4045891fecd3e4a
Claudon <106142715+NIYIGENAClaudine@users.noreply.github.com>
2024-06-12 15:09:11 +0200

    about

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+import { useState, useEffect } from "react";
 
-const Computers = () => {
+const Computers = (isMobile) => {
   const { scene } = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -20,8 +21,8 @@ const Computers = () => {
       />
       <primitive
         object={scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </>
@@ -29,6 +30,20 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       shadows
